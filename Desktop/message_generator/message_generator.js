761d0c235fb233aa6b0b6e51f36bbd0cdf96e7a9
gg2bds <gg2bds@gmail.com>
2022-08-10 14:47:14 +0800

    added function, loop in the msg array and get the element

--- Desktop/message_generator/message_generator.js
+++ Desktop/message_generator/message_generator.js
@@ -4,8 +4,35 @@ const messages ={
 	raisingLuck: ['Go with Friends', 'Relax', 'Stay Alone', 'EAT!', 'Listening to music you like', 'Do the thing you want']
 }
 
-function randMessage(){
-	return Math.floor(Math.random()*messages.length);
+function randMessage(num){
+	return Math.floor(Math.random()*num);
 }
 
-console.log(messages);
+let msgDialog = [];
+
+for(let msg in messages){
+	let index = randMessage(messages[msg].length);
+	
+	switch(msg){
+		case 'horoscope':
+			msgDialog.push(`${messages[msg][index]} is the lucky horoscope today.`);
+			break;
+		case 'luck':
+			msgDialog.push(`Today your luck is ${messages[msg][index]}`);
+			break;
+		case 'raisingLuck':
+			msgDialog.push(`${messages[msg][index]} can raise your luck today.`);
+			break;
+		default:
+			msgDialog.push('Nothing'); break;
+	}
+}
+
+console.log(msgDialog);
+
+function fommatMsg(msg){
+	const formatted = msgDialog.join('\n');
+	console.log(formatted);
+}
+
+fommatMsg(msgDialog);
